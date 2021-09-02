6c32c189f1db6737eef53551788de1e4e6c9e295
BryanDGuy <doughertybryan13@gmail.com>
2021-09-01 22:16:00 -0500

    separating things

--- App.tsx
+++ App.tsx
@@ -17,20 +17,15 @@ import Move from './src/lib/Move.enum';
 
 const App = () => {
   const [pickingFirstCard, setPickingFirstCard] = useState(true);
-  const [firstCard, setFirstCard] = useState(Card.ACE);
-  const [secondCard, setSecondCard] = useState(Card.FIVE);
-  const [dealerCard, setDealerCard] = useState(Card.ACE);
-
-  const decisionEngine = new DecisionEngine();
-
-  const hand = new Hand(firstCard, secondCard);
-  const nextMove = decisionEngine.decideMove(hand, dealerCard);
+  const [firstCard, setFirstCard] = useState(Card.TWO);
+  const [secondCard, setSecondCard] = useState(Card.TWO);
+  const [dealerCard, setDealerCard] = useState(Card.TWO);
 
   const isCardInHand = (card: Card) => {
     return firstCard === card || secondCard === card;
   };
 
-  const isCardInDealerHand = (card: Card) => {
+  const isDealerCard = (card: Card) => {
     return dealerCard === card;
   };
 
@@ -45,135 +40,148 @@ const App = () => {
     setPickingFirstCard(!pickingFirstCard);
   };
 
+  const decisionEngine = new DecisionEngine();
+
+  const hand = new Hand(firstCard, secondCard);
+  const nextMove = decisionEngine.decideMove(hand, dealerCard);
+
   return (
     <View>
-      <Text>
-        You: {hand.getValue()} ({hand.getName()})
-      </Text>
-      <Text>Dealer: {dealerCard}</Text>
-      <Text>Your next move should be {Move[nextMove]}</Text>
-      <Text>Pick your {pickingFirstCard ? 'first' : 'second'} card</Text>
-      <Button
-        color={isCardInHand(Card.TWO) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.TWO)}
-        title="2">
-        2
-      </Button>
-      <Button
-        color={isCardInHand(Card.THREE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.THREE)}
-        title="3">
-        3
-      </Button>
-      <Button
-        color={isCardInHand(Card.FOUR) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.FOUR)}
-        title="4">
-        4
-      </Button>
-      <Button
-        color={isCardInHand(Card.FIVE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.FIVE)}
-        title="5">
-        5
-      </Button>
-      <Button
-        color={isCardInHand(Card.SIX) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.SIX)}
-        title="6">
-        6
-      </Button>
-      <Button
-        color={isCardInHand(Card.SEVEN) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.SEVEN)}
-        title="7">
-        7
-      </Button>
-      <Button
-        color={isCardInHand(Card.EIGHT) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.EIGHT)}
-        title="8">
-        8
-      </Button>
-      <Button
-        color={isCardInHand(Card.NINE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.NINE)}
-        title="9">
-        9
-      </Button>
-      <Button
-        color={isCardInHand(Card.TEN) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.TEN)}
-        title="10">
-        10
-      </Button>
-      <Button
-        color={isCardInHand(Card.ACE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setHandCard(Card.ACE)}
-        title="A">
-        A
-      </Button>
-      <Text>Pick the dealer's card</Text>
-      <Button
-        color={isCardInDealerHand(Card.TWO) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.TWO)}
-        title="2">
-        2
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.THREE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.THREE)}
-        title="3">
-        3
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.FOUR) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.FOUR)}
-        title="4">
-        4
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.FIVE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.FIVE)}
-        title="5">
-        5
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.SIX) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.SIX)}
-        title="6">
-        6
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.SEVEN) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.SEVEN)}
-        title="7">
-        7
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.EIGHT) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.EIGHT)}
-        title="8">
-        8
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.NINE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.NINE)}
-        title="9">
-        9
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.TEN) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.TEN)}
-        title="10">
-        10
-      </Button>
-      <Button
-        color={isCardInDealerHand(Card.ACE) ? '#F194FF' : '#2196F3'}
-        onPress={() => setDealerCard(Card.ACE)}
-        title="A">
-        A
-      </Button>
+      <View>
+        <Text>
+          You: {hand.getValue()} ({hand.getName()})
+        </Text>
+        <Text>Dealer: {dealerCard}</Text>
+        <Text>Your next move should be {Move[nextMove]}</Text>
+      </View>
+
+      <View>
+        <Text>Pick your {pickingFirstCard ? 'first' : 'second'} card</Text>
+        <Button
+          color={isCardInHand(Card.TWO) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.TWO)}
+          title="2">
+          2
+        </Button>
+        <Button
+          color={isCardInHand(Card.THREE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.THREE)}
+          title="3">
+          3
+        </Button>
+        <Button
+          color={isCardInHand(Card.FOUR) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.FOUR)}
+          title="4">
+          4
+        </Button>
+        <Button
+          color={isCardInHand(Card.FIVE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.FIVE)}
+          title="5">
+          5
+        </Button>
+        <Button
+          color={isCardInHand(Card.SIX) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.SIX)}
+          title="6">
+          6
+        </Button>
+        <Button
+          color={isCardInHand(Card.SEVEN) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.SEVEN)}
+          title="7">
+          7
+        </Button>
+        <Button
+          color={isCardInHand(Card.EIGHT) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.EIGHT)}
+          title="8">
+          8
+        </Button>
+        <Button
+          color={isCardInHand(Card.NINE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.NINE)}
+          title="9">
+          9
+        </Button>
+        <Button
+          color={isCardInHand(Card.TEN) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.TEN)}
+          title="10">
+          10
+        </Button>
+        <Button
+          color={isCardInHand(Card.ACE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setHandCard(Card.ACE)}
+          title="A">
+          A
+        </Button>
+      </View>
+
+      <View>
+        <Text>Pick the dealer's card</Text>
+        <Button
+          color={isDealerCard(Card.TWO) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.TWO)}
+          title="2">
+          2
+        </Button>
+        <Button
+          color={isDealerCard(Card.THREE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.THREE)}
+          title="3">
+          3
+        </Button>
+        <Button
+          color={isDealerCard(Card.FOUR) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.FOUR)}
+          title="4">
+          4
+        </Button>
+        <Button
+          color={isDealerCard(Card.FIVE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.FIVE)}
+          title="5">
+          5
+        </Button>
+        <Button
+          color={isDealerCard(Card.SIX) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.SIX)}
+          title="6">
+          6
+        </Button>
+        <Button
+          color={isDealerCard(Card.SEVEN) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.SEVEN)}
+          title="7">
+          7
+        </Button>
+        <Button
+          color={isDealerCard(Card.EIGHT) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.EIGHT)}
+          title="8">
+          8
+        </Button>
+        <Button
+          color={isDealerCard(Card.NINE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.NINE)}
+          title="9">
+          9
+        </Button>
+        <Button
+          color={isDealerCard(Card.TEN) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.TEN)}
+          title="10">
+          10
+        </Button>
+        <Button
+          color={isDealerCard(Card.ACE) ? '#F194FF' : '#2196F3'}
+          onPress={() => setDealerCard(Card.ACE)}
+          title="A">
+          A
+        </Button>
+      </View>
     </View>
   );
 };
