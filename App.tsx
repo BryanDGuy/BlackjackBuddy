c2caf2e6136fee49e77fade20fc9218419237f66
BryanDGuy <doughertybryan13@gmail.com>
2021-09-01 22:57:00 -0500

    stylized

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, TouchableHighlight, StyleSheet} from 'react-native';
 import DecisionEngine from './src/lib/DecisionEngine';
 import Card from './src/lib/Card.enum';
 import Hand from './src/lib/Hand';
@@ -20,7 +20,6 @@ const App = () => {
   };
 
   const setHandCard = (card: Card) => {
-    console.log(card);
     if (pickingFirstCard) {
       setFirstCard(card);
     } else {
@@ -31,7 +30,6 @@ const App = () => {
   };
 
   const decisionEngine = new DecisionEngine();
-
   const hand = new Hand(firstCard, secondCard);
   const nextMove = decisionEngine.decideMove(hand, dealerCard);
 
@@ -46,131 +44,177 @@ const App = () => {
       </View>
 
       <View>
-        <Text>Pick your {pickingFirstCard ? 'first' : 'second'} card</Text>
-        <Button
-          color={isCardInHand(Card.TWO) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.TWO)}
-          title="2">
-          2
-        </Button>
-        <Button
-          color={isCardInHand(Card.THREE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.THREE)}
-          title="3">
-          3
-        </Button>
-        <Button
-          color={isCardInHand(Card.FOUR) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.FOUR)}
-          title="4">
-          4
-        </Button>
-        <Button
-          color={isCardInHand(Card.FIVE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.FIVE)}
-          title="5">
-          5
-        </Button>
-        <Button
-          color={isCardInHand(Card.SIX) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.SIX)}
-          title="6">
-          6
-        </Button>
-        <Button
-          color={isCardInHand(Card.SEVEN) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.SEVEN)}
-          title="7">
-          7
-        </Button>
-        <Button
-          color={isCardInHand(Card.EIGHT) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.EIGHT)}
-          title="8">
-          8
-        </Button>
-        <Button
-          color={isCardInHand(Card.NINE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.NINE)}
-          title="9">
-          9
-        </Button>
-        <Button
-          color={isCardInHand(Card.TEN) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.TEN)}
-          title="10">
-          10
-        </Button>
-        <Button
-          color={isCardInHand(Card.ACE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setHandCard(Card.ACE)}
-          title="A">
-          A
-        </Button>
+        <Text style={styles.textPrompt}>
+          Pick your {pickingFirstCard ? 'first' : 'second'} card
+        </Text>
+        <View style={styles.cardContainer}>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.TWO) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.TWO)}
+              title="2">
+              2
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.THREE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.THREE)}
+              title="3">
+              3
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.FOUR) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.FOUR)}
+              title="4">
+              4
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.FIVE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.FIVE)}
+              title="5">
+              5
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.SIX) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.SIX)}
+              title="6">
+              6
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.SEVEN) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.SEVEN)}
+              title="7">
+              7
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.EIGHT) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.EIGHT)}
+              title="8">
+              8
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.NINE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.NINE)}
+              title="9">
+              9
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.TEN) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.TEN)}
+              title="10">
+              10
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isCardInHand(Card.ACE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setHandCard(Card.ACE)}
+              title="A">
+              A
+            </Button>
+          </TouchableHighlight>
+        </View>
       </View>
 
       <View>
-        <Text>Pick the dealer's card</Text>
-        <Button
-          color={isDealerCard(Card.TWO) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.TWO)}
-          title="2">
-          2
-        </Button>
-        <Button
-          color={isDealerCard(Card.THREE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.THREE)}
-          title="3">
-          3
-        </Button>
-        <Button
-          color={isDealerCard(Card.FOUR) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.FOUR)}
-          title="4">
-          4
-        </Button>
-        <Button
-          color={isDealerCard(Card.FIVE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.FIVE)}
-          title="5">
-          5
-        </Button>
-        <Button
-          color={isDealerCard(Card.SIX) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.SIX)}
-          title="6">
-          6
-        </Button>
-        <Button
-          color={isDealerCard(Card.SEVEN) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.SEVEN)}
-          title="7">
-          7
-        </Button>
-        <Button
-          color={isDealerCard(Card.EIGHT) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.EIGHT)}
-          title="8">
-          8
-        </Button>
-        <Button
-          color={isDealerCard(Card.NINE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.NINE)}
-          title="9">
-          9
-        </Button>
-        <Button
-          color={isDealerCard(Card.TEN) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.TEN)}
-          title="10">
-          10
-        </Button>
-        <Button
-          color={isDealerCard(Card.ACE) ? '#F194FF' : '#2196F3'}
-          onPress={() => setDealerCard(Card.ACE)}
-          title="A">
-          A
-        </Button>
+        <Text style={styles.textPrompt}>Pick the dealer's card</Text>
+        <View style={styles.cardContainer}>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.TWO) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.TWO)}
+              title="2">
+              2
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.THREE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.THREE)}
+              title="3">
+              3
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.FOUR) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.FOUR)}
+              title="4">
+              4
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.FIVE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.FIVE)}
+              title="5">
+              5
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.SIX) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.SIX)}
+              title="6">
+              6
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.SEVEN) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.SEVEN)}
+              title="7">
+              7
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.EIGHT) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.EIGHT)}
+              title="8">
+              8
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.NINE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.NINE)}
+              title="9">
+              9
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.TEN) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.TEN)}
+              title="10">
+              10
+            </Button>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.touchable}>
+            <Button
+              color={isDealerCard(Card.ACE) ? '#F194FF' : '#2196F3'}
+              onPress={() => setDealerCard(Card.ACE)}
+              title="A">
+              A
+            </Button>
+          </TouchableHighlight>
+        </View>
       </View>
     </View>
   );
@@ -180,6 +224,19 @@ const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
     marginVertical: 8,
+  },
+  textPrompt: {
+    textAlign: 'center',
+  },
+  cardContainer: {
+    padding: 2,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  touchable: {
+    width: 80,
+    padding: 1,
   },
 });
 
